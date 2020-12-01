47fa0b92ae58546323c33f8cc3332a3f24e742e7
htutwaiphyoe <htutwaiphyoe@gmail.com>
2020-12-01 14:19:37 +0630

    Adding Home Hero Description

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,34 +1,55 @@
+import { useCallback, useEffect, useRef } from "react";
 import { Link, NavLink } from "react-router-dom";
 
 import classes from "./Home.module.css";
 const Home = (props) => {
+    const heading = useRef();
+    let i = 0;
+    let txt = "NOT A OPTION"; /* The text */
+    let speed = 500; /* The speed/duration of the effect in milliseconds */
+    const typeWriter = () => {
+        if (heading.current) {
+            if (i < txt.length) {
+                heading.current.innerHTML += `${txt.charAt(i)}`;
+                i++;
+                setTimeout(typeWriter, speed);
+            } else {
+                i = 0;
+                heading.current.innerHTML = "IS ";
+                setTimeout(typeWriter, speed);
+            }
+        }
+    };
+    useEffect(() => {
+        typeWriter();
+    }, []);
     return (
         <header className={classes.Home}>
             <nav className={classes.Nav}>
                 <ul>
                     <li>
                         <NavLink to="/" activeClassName={classes.active}>
-                            HOME
+                            Home
                         </NavLink>
                     </li>
                     <li>
                         <NavLink to="/about" activeClassName={classes.active}>
-                            ABOUT
+                            About
                         </NavLink>
                     </li>
                     <li>
                         <NavLink to="/portfolio" activeClassName={classes.active}>
-                            PORTFOLIO
+                            Portfolio
                         </NavLink>
                     </li>
                     <li>
                         <NavLink to="/blogs" activeClassName={classes.active}>
-                            BLOGS
+                            Blogs
                         </NavLink>
                     </li>
                     <li>
                         <NavLink to="/contact" activeClassName={classes.active}>
-                            CONTACT
+                            Contact
                         </NavLink>
                     </li>
                 </ul>
@@ -36,10 +57,10 @@ const Home = (props) => {
             <div className={classes.Hero}>
                 <div>
                     <h1>GIVING UP</h1>
-                    <h2>IS NOT A OPTION</h2>
+                    <h2 ref={heading}>IS </h2>
                 </div>
-                <div>
-                    <Link to="/blogs">Read Articles</Link>
+                <div className={classes.Btn}>
+                    <Link to="/blogs">Discover Blogs</Link>
                 </div>
             </div>
         </header>
