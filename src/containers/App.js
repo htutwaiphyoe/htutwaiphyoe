15ddf04d357236629be81ef558b88c9ae84efa56
htutwaiphyoe <htutwaiphyoe@gmail.com>
2020-12-30 17:23:51 +0630

    #update: removing loader for fallback

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,43 +2,43 @@ import React, { Suspense } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import Layout from "./Layout/Layout";
-import Loader from "../components/UI/Loader/Loader";
+// import Loader from "../components/UI/Loader/Loader";
 import "./App.css";
 
 const Home = React.lazy(() => {
     return new Promise((resolve) => {
-        setTimeout(() => resolve(import("./Home/Home")), 1000);
+        setTimeout(() => resolve(import("./Home/Home")), 0);
     });
 });
 const Blog = React.lazy(() => {
     return new Promise((resolve) => {
-        setTimeout(() => resolve(import("./Blog/Blog")), 1000);
+        setTimeout(() => resolve(import("./Blog/Blog")), 0);
     });
 });
 const FullBlog = React.lazy(() => {
     return new Promise((resolve) => {
-        setTimeout(() => resolve(import("./FullBlog/FullBlog")), 1000);
+        setTimeout(() => resolve(import("./FullBlog/FullBlog")), 0);
     });
 });
 const Contact = React.lazy(() => {
     return new Promise((resolve) => {
-        setTimeout(() => resolve(import("./Contact/Contact")), 1000);
+        setTimeout(() => resolve(import("./Contact/Contact")), 0);
     });
 });
 const About = React.lazy(() => {
     return new Promise((resolve) => {
-        setTimeout(() => resolve(import("./About/About")), 1000);
+        setTimeout(() => resolve(import("./About/About")), 0);
     });
 });
 const Portfolio = React.lazy(() => {
     return new Promise((resolve) => {
-        setTimeout(() => resolve(import("./Portfolio/Portfolio")), 1000);
+        setTimeout(() => resolve(import("./Portfolio/Portfolio")), 0);
     });
 });
 
 const MessageBox = React.lazy(() => {
     return new Promise((resolve) => {
-        setTimeout(() => resolve(import("../components/UI/MessageBox/MessageBox")), 1000);
+        setTimeout(() => resolve(import("../components/UI/MessageBox/MessageBox")), 0);
     });
 });
 
@@ -46,7 +46,7 @@ const App = (props) => {
     return (
         <BrowserRouter>
             <Layout>
-                <Suspense fallback={<Loader />}>
+                <Suspense fallback={null}>
                     <Switch>
                         <Route path="/" exact component={Home} />
                         <Route path="/blogs" exact component={Blog} />
